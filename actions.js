a281900b099ce89ecf3ff418f11d34cff9b4f1a5
Susie Gordon <susieyoungchoi@gmail.com>
2023-08-19 12:15:54 -0400

    added actions

---
+++ actions.js
@@ -0,0 +1,65 @@
+import { baseUrl } from "./base_url";
+import { redirect } from "react-router-dom";
+
+export const createAction = async ({request}) => {
+    // get the data from the form in the request
+    const formData = await request.formData()
+    const newEntry = {
+        location: formData.get('location'),
+        landmark: formData.get('landmark'),
+        image: formData.get('image'),
+        dates: formData.get('dates')
+    }
+    // send the new entry to our backend API 
+    await fetch (`${baseUrl}/travel`, {
+        // tell fetch to make a post request
+        method: "POST",
+        headers: {
+            // tells our backend the data is json
+            "Content-Type": "application/json"
+        },
+        // send the json string of the newEntry object
+        body: JSON.stringify(newEntry)
+    })
+    // redirect the user back to the frontend index route
+    return redirect('/')
+};
+
+export const updateAction = async ({request, params}) => {
+    // grab the id from the params
+    const id = params.id
+    // grab the form data from the form
+    const formData = await request.formData()
+    // build out the updated person
+    const updatedEntry = {
+        location: formData.get('location'),
+        landmark: formData.get('landmark'),
+        image: formData.get('image'),
+        dates: formData.get('dates')
+    }
+    // send the updated person to our backend API
+    await fetch (`${baseUrl}/travel/${id}`, {
+        // tell fetch to make a put request
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        // send the json string of the updatedEntry object
+        body: JSON.stringify(updatedEntry)
+    })
+    // redirect back to show page fronend route
+    return redirect(`/${id}`)
+};
+
+export const deleteAction = async ({params}) => {
+    // grab the id from the params
+    const id = params.id
+    // send the delete request to backend API
+    await fetch (`${baseUrl}/travel/${id}`, {
+        // tell fetch to make a delete request
+        method: "DELETE"
+        //no headers or body required 
+    })
+    // redirect back to the frontend index route
+    return redirect('/')
+};
