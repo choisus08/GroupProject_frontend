8c136e1b85709de68ce67538daa47c329e397b27
nicholasjamessmith <njsmith6@gmail.com>
2023-08-19 14:19:40 -0400

    show page complete

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,9 +1,18 @@
 import { Link, useLoaderData, Form } from "react-router-dom";
 
 function Show(props) {
-    return (
-        <h3>Show</h3>
-    )
-}
+  const entry = useLoaderData()
 
+  return (
+    <div className="show">
+      <h4>{entry.location}</h4>
+      <h4>{entry.landmark}</h4>
+      <img src={entry.landmark} alt={entry.location} />
+      <h4>{entry.dates}</h4>
+      <Form action={`/delete/${entry._id}`} method="post">
+        <input type="submit" value="Delete Entry" />
+      </Form>
+    </div>
+  )
+}
 export default Show;
