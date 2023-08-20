0c81b533d8aa056056517148d823d30c580c1f37
Susie Gordon <susieyoungchoi@gmail.com>
2023-08-20 19:45:06 -0400

    initialized styling, added background image -change entries to grid display later

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -5,26 +5,30 @@ function Index(props) {
   const entries = useLoaderData()
   return (
     <div>
-      <h2 className="indexForm">New Entry</h2>
-      <Form className="indexForm" action="/create" method="post">
+      <div className="indexForm">
+      <h2>New Entry</h2>
+      <Form action="/create" method="post">
         <input type="text" name="location" placeholder="Location" />
         <input type="text" name="landmark" placeholder="Landmark" />
         <input type="text" name="image" placeholder="Picture" />
         <input type="text" name="dates" placeholder="Dates" />
         <input type="submit" value="Create Entry" />
       </Form>
-      {entries.map((entry, index) => {
-        return (
-            <div key={entry._id}>
-              <Link to={`/${entry._id}`}>
-                <h4>{entry.location}</h4>
-              </Link>
-              <h4>{entry.landmark}</h4>
-              <img src={entry.image} alt={entry.location} />
-              <h4>{entry.dates}</h4>
-            </div>
-        )
-      })}
+      </div>
+      <div className="entries">
+        {entries.map((entry, index) => {
+          return (
+              <div key={entry._id}>
+                <Link to={`/${entry._id}`}>
+                  <h4>{entry.location}</h4>
+                </Link>
+                <h4>{entry.landmark}</h4>
+                <img src={entry.image} alt={entry.location} />
+                <h4>{entry.dates}</h4>
+              </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
