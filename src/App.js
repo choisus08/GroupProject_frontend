82c8040d718ef4dd86cbaf95a28cb966d01f6796
Susie Gordon <susieyoungchoi@gmail.com>
2023-08-21 12:16:30 -0400

    installed Lottie to use animation, will make changes later

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from './components/Header';
+import Sun from './Sun.json';
+import Lottie from "lottie-react"
 
 function App() {
   return (
     <div className="App">
+      <Lottie className="sunAnimation" animationData={Sun} />
       <Header />
       <Outlet />
     </div>
