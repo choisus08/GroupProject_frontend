1b28c573ad345b5caeff816577991edcb8d24c3e
nicholasjamessmith <njsmith6@gmail.com>
2023-08-21 18:42:33 -0400

    added onclick function to Index

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,31 +1,37 @@
-import { Link, useLoaderData, Form } from "react-router-dom";
+import { Link, useLoaderData, Form, useNavigate } from "react-router-dom";
+import { baseUrl } from "../base_url";
 
 function Index(props) {
-  //check variable name for consistency
-  const entries = useLoaderData()
+  const entries = useLoaderData();
+  const navigate = useNavigate();
   return (
     <div>
+      <button onClick={async () => {
+        await fetch(`${baseUrl}/logout`)
+        localStorage.removeItem('logedIn')
+        navigate("/")
+      }}>Logout</button>
       <div className="indexForm">
-      <h2>New Entry</h2>
-      <Form action="/create" method="post">
-        <input type="text" name="location" placeholder="Location" />
-        <input type="text" name="landmark" placeholder="Landmark" />
-        <input type="text" name="image" placeholder="Picture" />
-        <input type="text" name="dates" placeholder="Dates" />
-        <input type="submit" value="Create Entry" />
-      </Form>
+        <h2>New Entry</h2>
+        <Form action="/create" method="post">
+          <input type="text" name="location" placeholder="Location" />
+          <input type="text" name="landmark" placeholder="Landmark" />
+          <input type="text" name="image" placeholder="Picture" />
+          <input type="text" name="dates" placeholder="Dates" />
+          <input type="submit" value="Create Entry" />
+        </Form>
       </div>
       <div className="entries">
         {entries.map((entry, index) => {
           return (
-              <div key={entry._id}>
-                <Link to={`/${entry._id}`}>
-                  <h4>{entry.location}</h4>
-                </Link>
-                <h4>{entry.landmark}</h4>
-                <img src={entry.image} alt={entry.location} />
-                <h4>{entry.dates}</h4>
-              </div>
+            <div key={entry._id}>
+              <Link to={`/${entry._id}`}>
+                <h4>{entry.location}</h4>
+              </Link>
+              <h4>{entry.landmark}</h4>
+              <img src={entry.image} alt={entry.location} />
+              <h4>{entry.dates}</h4>
+            </div>
           )
         })}
       </div>
