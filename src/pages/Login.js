4819a3cdbf0e127466cc4970b6cf569aeb9919d9
nicholasjamessmith <njsmith6@gmail.com>
2023-08-21 18:17:34 -0400

    login password added

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,6 +6,8 @@ function Login(props) {
     <Form action="/login" method="post">
       <label>Username</label>
       <input type="text" name="username" placeholder="username" />
+      <label>Password</label>
+      <input type="password" name="password" placeholder="password" />
       <input type="submit" value="Login" />
     </Form>
   </div>
