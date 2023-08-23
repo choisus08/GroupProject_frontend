f7399118abd183fcaff6c286e67ef8d921820faf
Auryx <131199997+Auryx@users.noreply.github.com>
2023-08-23 16:31:50 -0700

    Update base_url.js

--- src/base_url.js
+++ src/base_url.js
@@ -1,2 +1,2 @@
-export const baseUrl = "http://groupproject-travel.onrender.com"
+export const baseUrl = "https://groupproject-travel.onrender.com"
 // export const baseUrl = "http://localhost:6060"
