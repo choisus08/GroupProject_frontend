1f892d45a4c26f41dbd56fadb601da4925d3ddff
Susie Gordon <susieyoungchoi@gmail.com>
2023-08-23 15:08:15 -0400

    added more css to main.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Header(props) {
   return <nav className="nav">
-    <Link to="/">
+    <Link style={{textDecoration: "none", color: "rgb(27, 104, 155)"}} to="/">
       <div>Travelog</div>
     </Link>
   </nav>
