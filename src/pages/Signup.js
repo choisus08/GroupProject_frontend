618454bd58d4487c226b355114cd4f61a0d633fa
Susie Gordon <susieyoungchoi@gmail.com>
2023-08-23 20:37:55 -0400

    added styling to login and signup forms

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -3,12 +3,10 @@ import { Form } from "react-router-dom";
 function Signup(props) {
   return <div className="signupForm">
     <h3>Sign Up</h3>
-    <Form action="/signup" method="POST">
-      <label htmlFor="username">Username</label>
-      <input type="text" name="username" id="username" />
-      <label htmlFor="password">Password</label>
-      <input type="password" name="password" id="password" />
-      <input type="submit" value="Signup" />
+    <Form className="signupInput" action="/signup" method="POST">
+      <input className="input" type="text" name="username" id="username" placeholder="Username"/>
+      <input className="input" type="password" name="password" id="password" placeholder="Password"/>
+      <input className="signupButton" type="submit" value="Signup" />
     </Form>
   </div>
 }
