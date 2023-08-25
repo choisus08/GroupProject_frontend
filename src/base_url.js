594dc7aaba67e74ab580ea092cc99b1b995b4b88
nicholasjamessmith <njsmith6@gmail.com>
2023-08-25 16:40:27 -0400

    baseUrl changed

--- src/base_url.js
+++ src/base_url.js
@@ -1,3 +1,3 @@
-//export const baseUrl = "https://groupproject-travel.onrender.com"
- export const baseUrl = "http://localhost:6060"
+export const baseUrl = "https://groupproject-travel.onrender.com"
+// export const baseUrl = "http://localhost:6060"
 
